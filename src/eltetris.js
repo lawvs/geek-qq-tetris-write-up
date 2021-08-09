d87636e7ecfb19b0a8f43d03dd6fcf7cf3ccb7e3
lawvs <18554747+lawvs@users.noreply.github.com>
2021-08-10 01:22:11 +0800

    feat: use new strategy

--- src/eltetris.js
+++ src/eltetris.js
@@ -14,30 +14,6 @@ let RowsRemovedFactor = 3.4181268101392694;
 ElTetris.prototype.evaluateBoard = function (last_move, board) {
   const count = getCount();
 
-  if (last_move.landing_height < 10) {
-    LandingHeightFactor = -4;
-  }
-  if (last_move.landing_height < 5) {
-    LandingHeightFactor = -2;
-  }
-  if (clamp(3000, 3500)) {
-    LandingHeightFactor = -6;
-  }
-  if (clamp(4900, 5000)) {
-    LandingHeightFactor = -7;
-  }
-  if (clamp(6300, 6500)) {
-    LandingHeightFactor = -7;
-  }
-
-  if (clamp(7500, 7600)) {
-    LandingHeightFactor = -9;
-  }
-
-  if (clamp(9000, 9200)) {
-    LandingHeightFactor = -7;
-  }
-
   return (
     GetLandingHeight(last_move, board) * LandingHeightFactor +
     last_move.rows_removed * RowsRemovedFactor +
@@ -46,4 +22,60 @@ ElTetris.prototype.evaluateBoard = function (last_move, board) {
     GetNumberOfHoles(board, this.number_of_columns) * -7.899265427351652 +
     GetWellSums(board, this.number_of_columns) * -3.3855972247263626
   );
+};
+
+ElTetris.prototype.pickMove = function (piece) {
+  const worst = {
+    evaluation: -100000,
+    orientation: piece[0].orientation,
+    column: 0,
+    removed: 0,
+    landing_height: Infinity,
+  };
+  let candidates = [worst];
+  let maxLandingHeight = -Infinity;
+  // Evaluate all possible orientations
+  for (var i in piece) {
+    var orientation = piece[i].orientation;
+
+    // Evaluate all possible columns
+    for (var j = 0; j < this.number_of_columns - piece[i].width + 1; j++) {
+      // Copy current board
+      var board = this.board.slice();
+      var last_move = this.playMove(board, orientation, j);
+
+      if (!last_move.game_over) {
+        const evaluation = this.evaluateBoard(last_move, board);
+
+        maxLandingHeight = Math.max(maxLandingHeight, last_move.landing_height);
+
+        candidates.push({
+          evaluation,
+          orientation: piece[i].orientation,
+          column: j,
+          removed: last_move.rows_removed,
+          landing_height: last_move.landing_height,
+        });
+      }
+    }
+  }
+
+  candidates.sort((a, b) => b.evaluation - a.evaluation);
+  if (
+    maxLandingHeight >= 10 ||
+    clamp(400, 500) ||
+    clamp(1100, 1200) ||
+    clamp(3700, 4000) ||
+    clamp(4800, 5200) ||
+    clamp(5900, 6000)
+  ) {
+    return candidates[0];
+  }
+  const target = candidates.slice(0, 5).find((i) => !i.removed);
+  return target || candidates[0];
+
+  // return {
+  //   orientation: piece[best_orientation].orientation,
+  //   column: best_column,
+  // };
 };
